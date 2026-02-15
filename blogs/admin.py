954e8966bc5d87a57519f90a14f3473b2150c37e
Akankhya-Dash <akankhyapvt@gmail.com>
2026-02-15 06:27:26 +0530

    auto-slug generation enabled

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from . models import Category
+from . models import Category,Blog
+
+
+class BlogAdmin (admin.ModelAdmin):
+    prepopulated_fields={'slug':('title',)}
+
 admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
 # Register your models here.
