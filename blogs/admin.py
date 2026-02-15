14b22ed7f3dc3c4cbeaf545191a5a49e3bf37b26
Akankhya-Dash <akankhyapvt@gmail.com>
2026-02-15 07:38:55 +0530

    added admin table in list display format with editable tables

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,7 +4,10 @@ from . models import Category,Blog
 
 class BlogAdmin (admin.ModelAdmin):
     prepopulated_fields={'slug':('title',)}
-
+    list_display=('title','category','author','is_featured','status')
+    search_fields=('id','title','category__category_name','status__icontains')
+    
+    list_editable=('is_featured',)
 admin.site.register(Category)
 admin.site.register(Blog,BlogAdmin)
 # Register your models here.
