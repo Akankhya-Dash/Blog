7a29d9bc8fabb21976390140d46d591b5b67ec39
Akankhya-Dash <akankhyapvt@gmail.com>
2026-02-17 12:34:11 +0530

    added post_by_category page with its functionalities

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse    
+from blogs.models import Blog,Category
+def posts_by_category(request,category_id):
+    #fetch the post that belong to the category with the id category_id
+    posts=Blog.objects.filter(status='Published',category=category_id)
+    #use try catch if u want to show some action if category doesnt exist
+    try:
+      category=Category.objects.get(pk=category_id)
+    except:
+      #redirect the user to home page
+      return redirect('home')
+    #use get_object_or_404 where you want to show 404 error page is the category doesnt exist
+    #category=get_object_or_404(Category,pk=category_id)
+    context={
+        'posts':posts,
+        'category':category,
+    }
+    return render(request,'posts_by_category.html',context)
