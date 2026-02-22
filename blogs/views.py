37a963baf261ddcfb6956426d71f03e7e295b9ce
Akankhya-Dash <akankhyapvt@gmail.com>
2026-02-23 05:27:00 +0530

    added search page and its functionalities

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse    
 from blogs.models import Blog,Category
+from django.db.models import Q
+
 def posts_by_category(request,category_id):
     #fetch the post that belong to the category with the id category_id
     posts=Blog.objects.filter(status='Published',category=category_id)
@@ -24,3 +26,11 @@ def blogs(request,slug):
         'single_blog': single_blog ,
     }
     return render (request,'blogs.html',context)
+
+def search(request):
+    keyword=request.GET.get('keyword')
+    blogs=Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword) ,status='Published')
+    context={
+        'blogs':blogs,
+    }
+    return render(request,'search.html',context)
