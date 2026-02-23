8fdb56d4a1ead8680bd5c46f00e24a2cc8f941c9
Akankhya-Dash <akankhyapvt@gmail.com>
2026-02-23 05:51:44 +0530

    search layout and textbox features

--- blogs/views.py
+++ blogs/views.py
@@ -32,5 +32,6 @@ def search(request):
     blogs=Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword) ,status='Published')
     context={
         'blogs':blogs,
+        'keyword':keyword,
     }
     return render(request,'search.html',context)
