10939558191d027449cdb7c8757ccbacac4f025c
Akankhya-Dash <akankhyapvt@gmail.com>
2026-02-28 07:26:54 +0530

    authentication and authorization

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from blogs.models import Category,Blog
 from assignments.models import About
 from . forms import RegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 def home(request):
     featured_posts=Blog.objects.filter(is_featured=True,status='Published').order_by('updated_at')
     posts=Blog.objects.filter(is_featured=False,status='Published')
@@ -36,8 +37,22 @@ def register(request):
     return render(request,'register.html',context)
 
 def login(request):
-    form=AuthenticationForm()
-    context={
+     if request.method=='POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            
+            user=auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+                return redirect('home')
+     form=AuthenticationForm()
+     context={
         'form':form,
     }
-    return render(request,'login.html',context)
+     return render(request,'login.html',context)
+ 
+def logout(request):
+    auth.logout(request)
+    return redirect('home') 
